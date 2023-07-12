b7eb9ac4fa59180acff2dd22c479ff4679505055
idkhow2type <lecaominhhn@gmail.com>
2023-07-12 17:00:25 +0700

    fix get user thread

--- User.ts
+++ User.ts
@@ -2,6 +2,11 @@ import { parse } from 'node-html-parser';
 import { request, GenericInitialise } from './utils.js';
 import { Headers } from 'node-fetch';
 
+interface ThreadInfo {
+    id: string;
+    name: string;
+}
+
 export default class User extends GenericInitialise {
     private _session: string;
     private _uid: string;
@@ -11,7 +16,7 @@ export default class User extends GenericInitialise {
         this._session = session;
     }
     async initialise() {
-        super.initialise()
+        super.initialise();
         const page = parse(
             await (
                 await request('', 'GET', {
@@ -41,8 +46,13 @@ export default class User extends GenericInitialise {
      * getUserThreads
      */
     public async getUserThreads() {
-        request('get_user_cbat', 'POST', {
-            body: JSON.stringify({ user_id: this.uid }),
-        });
+        const infos: Array<any> = await (
+            await request('get_user_cbat', 'POST', {
+                body: JSON.stringify({ user_id: this.uid }),
+            })
+        ).json();
+        return infos.map(
+            (info) => ({ id: info._id, name: info.chat_name } as ThreadInfo)
+        );
     }
 }
